4441d1aac0e9cbb3609b3518a6763832a349b8a9
Bobby <bobbyc@Bobbys-MacBook-Pro.local>
2025-07-04 14:47:26 -0400

    fix: remove duplicate UserSchema

--- models/User.js
+++ models/User.js
@@ -1,26 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-}, { timestamps: true });
-
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-UserSchema.methods.comparePassword = function(candidate) {
-  return bcrypt.compare(candidate, this.password);
-};
-
-module.exports = mongoose.model('User', UserSchema);
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
 // 1. Define the schema
 const UserSchema = new mongoose.Schema({
   name:   { type: String, required: true },
